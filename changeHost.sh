8778d0b3aae34d707169fcae7384d5eefaf5daef
harsharns <76463042+harsharns@users.noreply.github.com>
2021-07-22 21:42:06 +0530

    Update changeHost.sh

--- changeHost.sh
+++ changeHost.sh
@@ -1,7 +1,8 @@
 #! bin/bash
 
+
 sed -i "s/host/$1/g" inventory
 
-sed -i "s/host-user/$2/g" inventory
+sed -i "s/windowsusr/$2/g" inventory
 
-sed -i "s/host-password/$3/g" inventory
+sed -i "s/adminpasscode/$3/g" inventory
